7a6049903ba649082e9afbb7c708a87bd21e0ebd
scottdouglas238 <scottdouglas238@gmail.com>
2024-04-24 22:20:14 -0700

    Added spacing to dynamically appended buttons

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const test_questions = [
 const test_answers = [
     {
         "id": 1,
-        "answers": ["It isn't", "Cohesion and adhesion", "It hasn't figured out how to be dry yet", "Taking a shower"],
+        "answers": ["It isn't", "Cohesion and adhesion", "It hasn't figured out how to be dry yet", "I was taking a shower"],
         "correct answer": 1
     },
     {
@@ -70,21 +70,23 @@ let answersIndex =  0
 const append_buttons = () => {
     let answersId = test_answers[answersIndex]
     const append_buttons = document.getElementById("append_buttons")
+    const rowDiv = document.createElement("div")
+    rowDiv.setAttribute("class", "row")
     
     append_buttons.innerHTML = ''
     
-    // const answer_buttons = document.createElement("button")
-    // answer_buttons.setAttribute("class", "btn btn-dark")
-    // answer_buttons.appendChild(document.createTextNode(answersId.answers[0]))
-    // append_buttons.parentNode.replaceChild(answer_buttons, append_buttons)
-    
     for (let i = 0; i < answersId.answers.length; i++) {
         const all_buttons = answersId.answers[i]
+        const colDiv = document.createElement("div")
+        colDiv.setAttribute("class", "col")
         const answer_buttons = document.createElement("button")
         answer_buttons.setAttribute("class", "btn btn-dark")
         answer_buttons.textContent = all_buttons
         
-        append_buttons.appendChild(answer_buttons)
+        append_buttons.appendChild(rowDiv)
+        rowDiv.appendChild(colDiv)
+        colDiv.appendChild(answer_buttons)
+        
     }
     
     answersIndex++
@@ -92,25 +94,3 @@ const append_buttons = () => {
 
 }
 
-// console.log(test_answers[0].answers[0])
-// const append_buttons = () => {
-//     const start_text = document.getElementById("intro")
-//     const h2Tag = document.createElement("h2")
-//     const original_button = document.getElementById("btn")
-//     const question_div = document.createElement("div")
-//     const first_button_test = document.createElement("div")
-
-//     h2Tag.innerHTML = test_question[0]
-//     first_button_test.innerHTML = ta[0]
-
-//     start_text.parentNode.replaceChild(h2Tag, start_text)
-//     question_div.setAttribute("class", "row")
-//     first_button_test.setAttribute("type", "button")
-//     first_button_test.setAttribute("class", "btn btn-secondary")
-//     original_button.parentNode.replaceChild(first_button_test, original_button)
-//     // for (let i = 0; i < test_answers.length; i++) {
-//     //     const ta = test_answers[i]
-//     //     ;
-//     // }
-//     console.log("initial button works")
-//}
